919fdd627f80c2a51f0e2d174261199cf6254d70
Gluraki <gluraki.spamm@gmail.com>
2025-12-06 15:08:01 +0100

    import driver api

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.routes import auth, user, protected, team
+from app.api.routes import auth, user, protected, team, driver
 
 api_router = APIRouter()
 
@@ -7,3 +7,4 @@ api_router.include_router(auth.router, prefix="/auth", tags=["auth"])
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(protected.router, prefix="", tags=["protected"])
 api_router.include_router(team.router, prefix="/teams", tags=["team"])
+api_router.include_router(driver.router, prefix="/drivers", tags=["driver"])
