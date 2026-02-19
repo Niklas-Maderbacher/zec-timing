6b53cda421299b00bb19d486f27523fc0c30f73d
Niklas-Maderbacher <maderb190007@sr.htlweiz.at>
2026-02-19 17:30:00 +0100

    CORS error need to be fixed

--- zec_api/services/auth-service/app/models/user.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class UserRole(Enum):
-    ADMIN = "ADMIN"
-    TEAM_LEAD = "TEAM_LEAD"
-    VIEWER = "VIEWER"
--- zec_api/services/user-service/app/api/main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from app.api.routes import user
-
-api_router = APIRouter()
-
-api_router.include_router(user.router, prefix="/users", tags=["users"])
