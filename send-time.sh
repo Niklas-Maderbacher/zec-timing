96780643f61ad88e887d823148d05d5d05fdea9c
Niklas-Maderbacher <maderb190007@sr.htlweiz.at>
2026-09-09 15:25:10 +0200

    Added timestamp expiretime as env var

--- send-time.sh
+++ send-time.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
-BROKER="172.31.177.41"
+
+BROKER="192.168.1.113"
 PORT="1883"
 TOPIC="esp32/timestamp"
 CLIENT_ID="raspi"
+
+MQTT_USER="zecuser"
+MQTT_PASSWORD="supersecret"
+
 ESP_ID="ESP32-$(cat /sys/class/net/wlo1/address)"
 
 # Generate timestamp in the required format with timezone
 TIMESTAMP=$(date +'%Y-%m-%dT%H:%M:%S.%6N%z')
 
-# JSON-Payload erzeugen
+# Generate JSON payload
 PAYLOAD=$(jq -n \
     --arg esp_id "$ESP_ID" \
     --arg timestamp "$TIMESTAMP" \
     '{esp_id: $esp_id, timestamp: $timestamp}')
 
-mosquitto_pub -h "$BROKER" -p "$PORT" -t "$TOPIC" -i "$CLIENT_ID" -m "$PAYLOAD"
+# Publish timestamp to MQTT broker
+mosquitto_pub \
+    -h "$BROKER" \
+    -p "$PORT" \
+    -t "$TOPIC" \
+    -i "$CLIENT_ID" \
+    -u "$MQTT_USER" \
+    -P "$MQTT_PASSWORD" \
+    -m "$PAYLOAD"
