153e303bcfd9713df156fb3aeda1dedba025ca1b
Niklas-Maderbacher <maderb190007@sr.htlweiz.at>
2026-09-09 15:14:17 +0200

    Automatic timestamp delete after 10 minutes

--- mqtt-worker/app/crud/timestamps.py
+++ mqtt-worker/app/crud/timestamps.py
@@ -3,26 +3,35 @@ from app.redis.redis import redis_connection, redis_lock
 from app.exceptions.mac_not_found import MacNotFound
 
 def add_timestamp(mac: str, timestamp: str):
-    redis_connection.rpush(mac, timestamp)
+    redis_connection.rpush(f"timestamps:{mac}", timestamp)
+    redis_connection.expire(f"timestamps:{mac}", 600)
 
 
 def get_timestamps(mac: str):
-    if not redis_connection.exists(mac):
+    key = f"timestamps:{mac}"
+
+    if not redis_connection.exists(key):
         raise MacNotFound(
-            message=("No ESP with this MAC address exists or no timestamps yet exist. "
-                "If you think it should exist, please send new MQTT data."),
-            error_code=404
+            message=(
+                "No ESP with this MAC address exists or no timestamps yet exist. "
+                "If you think it should exist, please send new MQTT data."
+            ),
+            error_code=404,
         )
 
-    return redis_connection.lrange(mac, 0, -1)
+    return redis_connection.lrange(key, 0, -1)
+
 
 def delete_timestamps(mac: str):
-    if not redis_connection.exists(mac):
+    key = f"timestamps:{mac}"
+
+    if not redis_connection.exists(key):
         raise MacNotFound(
-            message=("No ESP with this MAC address exists or no timestamps yet exist. "
-                "If you think it should exist, please send new MQTT data."),
-            error_code=404
+            message=(
+                "No ESP with this MAC address exists or no timestamps yet exist. "
+                "If you think it should exist, please send new MQTT data."
+            ),
+            error_code=404,
         )
 
-    # Delete all timestamps for this MAC
-    return redis_connection.ltrim(mac, 0, -1)
+    redis_connection.delete(key)
