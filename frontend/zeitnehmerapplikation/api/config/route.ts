0e294ca212427178f47fe08aad842441060b1ebf
maderbacher190007 <55692599+Niklas-Maderbacher@users.noreply.github.com>
2026-09-09 16:41:47 +0200

    Update GET response to use NextResponse

--- frontend/zeitnehmerapplikation/api/config/route.ts
+++ frontend/zeitnehmerapplikation/api/config/route.ts
@@ -1,6 +1,9 @@
+import { NextResponse } from "next/server";
+
 export async function GET() {
-    return Response.json({
-        desktopAppApiUrl: process.env.NEXT_PUBLIC_DESKTOP_APP_API_URL,
+    return NextResponse.json({
+        serverApiUrl: process.env.NEXT_PUBLIC_DESKTOP_APP_API_URL,
         mqttWorkerApiUrl: process.env.NEXT_PUBLIC_MQTT_WORKER_API_URL,
+        apiKey: process.env.NEXT_PUBLIC_API_KEY,
     });
 }
