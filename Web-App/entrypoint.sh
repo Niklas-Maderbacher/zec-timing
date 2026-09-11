55aa0ff38831ab45b45be782ae1f4c5b05d34de4
Niklas-Maderbacher <maderb190007@sr.htlweiz.at>
2026-09-11 13:22:47 +0200

    Env var injection

--- Web-App/entrypoint.sh
+++ Web-App/entrypoint.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
+
 set -e
 
-replace_env() {
-  var_name="$1"
-  placeholder="$2"
-  value=$(eval echo "\$$var_name")
-  if [ -n "$value" ]; then
-    find /app/.next /app/public -type f \( -name "*.js" -o -name "*.html" \) \
-      -exec sed -i "s|${placeholder}|${value}|g" {} +
-  fi
-}
-
-replace_env NEXT_PUBLIC_API_URL RUNTIME_PLACEHOLDER_API_URL
+cat <<EOF > /app/public/env.js
+window.__ENV__ = {
+  API_URL: "${NEXT_PUBLIC_API_URL}"
+};
+EOF
 
 exec "$@"
